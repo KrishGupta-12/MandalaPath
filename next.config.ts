22dcc721a0d25888ed361d13c9961241f1c5f718
Krish Gupta <shivkumar200510@gmail.com>
2025-10-03 13:19:32 +0000

    change the om symbol image, or remove its background and then use it

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.transparentpng.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
